23b4b9bfc417b3e39f5215ef5160d6bb9ad3361c
YU <yhj7124@naver.com>
2021-08-26 17:48:09 +0000

    plus model.py

---
+++ model.py
@@ -0,0 +1,38 @@
+def train_model(model, criterion, optimizer, num_epochs=5):
+    for epoch in tqdm(range(num_epochs)):
+        epoch_f1 = 0
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-' * 10)
+        
+        for phase in ['train', 'validation']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(image_datasets[phase])
+            epoch_acc = running_corrects.double() / len(image_datasets[phase])
+
+            print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,
+                                                        epoch_loss,
+                                                        epoch_acc))
+    return model
