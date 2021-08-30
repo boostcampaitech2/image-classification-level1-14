7e76a60ed0afe227bdf410916e1958c2bc21778c
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2021-08-30 22:57:24 +0900

    early stopping update

--- train.py
+++ train.py
@@ -236,6 +236,7 @@ def train(data_dir, model_dir, args):
             val_acc = np.sum(val_acc_items) / len(val_set)
             best_val_loss = min(best_val_loss, val_loss)
             if val_acc > best_val_acc:
+                early_stop_count=0
                 print(f"New best model for val accuracy : {val_acc:4.2%}! saving the best model..")
                 torch.save(model.module.state_dict(), f"{save_dir}/best.pth")
                 print('model path', f"{save_dir}")
@@ -255,7 +256,7 @@ def train(data_dir, model_dir, args):
             scheduler.step(val_loss)
             print()
 
-            if early_stop_count == 5:
+            if early_stop_count == 7:
                 break
                 
     logger.flush()
