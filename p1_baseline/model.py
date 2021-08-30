a2795cc2aaca8e0e9b7a33a4f6c6de82deb20387
nayoung <69383548+NayoungLee-de@users.noreply.github.com>
2021-08-30 15:01:42 +0900

    add freeze

--- p1_baseline/model.py
+++ p1_baseline/model.py
@@ -57,9 +57,11 @@ class efficient(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.net = EfficientNet.from_pretrained('efficientnet-b4')
-        num_feat = self.net._fc.in_features
-        self._fc = nn.Linear(num_feat, num_classes)
-         
+
+        for param in self.net.parameters():
+            param.requires_grad = False   
+        num_feat = self.net.fc.in_features
+        self.net.fc = nn.Linear(num_feat, num_classes)
 
     def forward(self, x):
         """
@@ -83,34 +85,4 @@ class resnet50(nn.Module):
         self.net.fc = nn.Linear(num_feat, num_classes)
 
     def forward(self, x):
-        return self.net(x)
-
-
-# Custom Model Template
-class VGG19(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
-        self.net = models.vgg19_bn(pretrained=True)
-        self.net.classifier = nn.Sequential(
-            nn.Linear(512*7*7,4896),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4896, 4896),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4896,num_classes)
-        )
-
-    def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-
         return self.net(x)
