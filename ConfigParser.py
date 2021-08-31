bf48609a4efb4900a1b85a947d336fe47e4d1cf1
YU <yhj7124@naver.com>
2021-08-31 18:14:51 +0000

    commit for pull

---
+++ ConfigParser.py
@@ -0,0 +1,25 @@
+import configparser
+## configparser load
+config = configparser.ConfigParser()
+config.read('config.cfg')
+if not config.sections():
+    raise Exception('config file is missing')
+
+
+seed=int(config['arg']['seed'])
+epochs=int(config['arg']['epochs'])
+dataset=config['arg']['dataset']
+augmentation=config['arg']['augmentation']
+resize=list(map(lambda x:int(x), config['arg']['resize'].split(', ')))
+batch_size=int(config['arg']['batch_size'])
+valid_batch_size=int(config['arg']['valid_batch_size'])
+model=config['arg']['model']
+optimizer=config['arg']['optimizer']
+lr=float(config['arg']['lr'])
+val_ratio=float(config['arg']['val_ratio'])
+criterion=config['arg']['criterion']
+lr_decay_step=int(config['arg']['lr_decay_step'])
+log_interval=int(config['arg']['log_interval'])
+name=config['arg']['name']
+model_name = config['arg']['model_name'] # choose one of model_names in timm.list_models(pretrained=True)
+pretrained = bool(config['arg']['pretrained'])
